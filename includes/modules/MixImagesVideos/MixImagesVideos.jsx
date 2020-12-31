71e217988a86b77ed7923c050b86d2ba4cd289bf
Hieuhuynh <conghieuhuynh91@gmail.com>
2020-12-31 10:44:01 +0700

    style module mix image videos

--- includes/modules/MixImagesVideos/MixImagesVideos.jsx
+++ includes/modules/MixImagesVideos/MixImagesVideos.jsx
@@ -1,6 +1,7 @@
 // External Dependencies
 import React, { Component } from 'react';
 
+
 // Internal Dependencies
 import './style.scss';
 
@@ -10,21 +11,41 @@ class MixImagesVideos extends Component {
   static slug = 'dipp_mix_images_videos';
 
   render() {
-    const src = this.props.src;
-    const name = this.props.name;
-    const position = this.props.position;
-    const email = this.props.email;
+    const heading = this.props.heading;
+    const description = this.props.description;
+    const src_video = this.props.src_video;
+    const src_img_1 = this.props.src_img_1;
+    const src_img_2 = this.props.src_img_2;
     return (
-      <div className="dipp-module-member-team">
-        <div className="wrapper-content-team-member">
-          <div className="img-member" style={{backgroundImage: `url(${src})`}}></div>          
-          <h5 className="name-member">{name}</h5>
-          <p className="position-member">{position}</p>
-          <p className="email-member">{email}</p>
+      <div className="dipp-module-mix-image-videos">
+        <div className="wrapper-content-mix-images-videos">
+          <div className="content-rows">
+            <div className="mix-images-videos-col">
+              <div className="title-header">
+                <h2>{heading}</h2>
+              </div>
+              <div claclassNamess="desc-content">{description}</div>
+            </div>
+            <div className="mix-images-videos-col">
+              <div className="wrap-videos">
+                <video
+                  autoPlay
+                  src={this.props.src_video} />
+              </div>
+            </div>
+          </div>
+          <div className="wrap-images">
+            <div className="module-mix-images mix-images-1">
+              <img src={src_img_1} alt="image 1" />
+            </div>
+            <div className="module-mix-images mix-images-2">
+              <img src={src_img_2} alt="image 2" />
+            </div>
+          </div>
         </div>
       </div>
     );
   }
 }
 
-export default MixImagesVideos ;
+export default MixImagesVideos;
